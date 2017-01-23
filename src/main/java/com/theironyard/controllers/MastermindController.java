56323ca7a2014c699fa70f4493f94c46a3aa4dc1
Ser-Castic <samsteinberg90@gmail.com>
2017-01-23 16:21:19 -0500

    Fixed the numbeGenerator from producing only 1's lol, Added a non null checks array, added a check against the answer method

--- src/main/java/com/theironyard/controllers/MastermindController.java
+++ src/main/java/com/theironyard/controllers/MastermindController.java
@@ -20,52 +20,46 @@ public class MastermindController {
     @Autowired
     MastermindRepository games;
 
-//    @PostConstruct
-//    public void init() {
-//        if(games.count() == 0) { //If this is either the start of the game or a new game, this is run to create the answer
-//            Mastermind master = new Mastermind();
-//            master.setRound(0);
-//            master.setGuesses(new int[]{numberGenerator(), numberGenerator(), numberGenerator(), numberGenerator()}); //creates the answer
-//            games.save(master);
-//        }
-//    }
-
-    public static int numberGenerator() { // gets a number between 1 - 8 randomly
-        return ((int)Math.random() * 8) + 1;
+    @PostConstruct
+    public void init() {
+        if(games.count() == 0) { //If this is either the start of the game or a new game, this is run to create the answer
+            Mastermind master = new Mastermind();
+            master.setRound(0);
+            master.setGuesses(new int[]{numberGenerator(), numberGenerator(), numberGenerator(), numberGenerator()});//creates the answer
+            master.setChecks(new int[]{0,0,0,0});
+            games.save(master);
+        }
     }
 
-    //visualization
-    //{1, 2, 4, 5} answer
-    //{1, 4, 6, 7} guess
-//    public int[] checkAgainstAnswer(int[] guessArray) { // this checks
-//        Mastermind answerMaster = games.findOne(0); // sets first position in repo to this object
-//        int [] answerArray = answerMaster.getGuesses();// gets the guess array field and sets it to this variable
-//        int [] result = new int[guessArray.length]; // created result array to be returned
-//        boolean areEqual = Arrays.equals(answerArray, guessArray);
-//
-//        if(areEqual) {
-//            for (int i = 0; i < guessArray.length; i++) {
-//                result[i] = 2;
-//            }
-//            return result;
-//        }
+    public static int numberGenerator() { // gets a number between 1 - 8 randomly
+        return (int)(Math.random() * 8) + 1;
+    }
 
+    public static int[] checkAgainstAnswer(int[] answerArray, int[] guessArray) { // this checks
+        answerArray = Arrays.copyOf(answerArray, answerArray.length);
 
-//        for (int i = 0; i < answerArray.length; i++) { // loop through both arrays comparing
-//            for (int x = 0; x < guessArray.length; x++) {
-//                if (answerArray[i] == guessArray[x] && flag == true) {
-//                    result[i] = 2;
-//                } else if ()
-//            }
-//        }
+        int[] result = new int[answerArray.length];
 
-//    }
+        for (int i = 0; i < result.length; i++) {
+            if (answerArray[i] == guessArray[i]) {
+                result[i] = 2;
+                answerArray[i] = 0;
+                continue;
+            }
+
+            int answerIndex = Arrays.binarySearch(answerArray, guessArray[i]);
+
+            if(answerIndex > -1) {
+                result[i] = 1;
+                answerArray[answerIndex] = 0;
+            }
+        }
+        return result;
+    }
 
     @CrossOrigin
     @RequestMapping(path = "/", method = RequestMethod.GET)
     public String home() {
         return "Pants";
     }
-
-
 }
