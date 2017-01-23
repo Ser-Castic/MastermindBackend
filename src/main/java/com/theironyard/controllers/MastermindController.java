259fc0b4b46d41abf09cddf36fe2d1890f275777
Ser-Castic <samsteinberg90@gmail.com>
2017-01-23 16:08:41 -0500

    Removing postconstruct

--- src/main/java/com/theironyard/controllers/MastermindController.java
+++ src/main/java/com/theironyard/controllers/MastermindController.java
@@ -20,15 +20,15 @@ public class MastermindController {
     @Autowired
     MastermindRepository games;
 
-    @PostConstruct
-    public void init() {
-        if(games.count() == 0) { //If this is either the start of the game or a new game, this is run to create the answer
-            Mastermind master = new Mastermind();
-            master.setRound(0);
-            master.setGuesses(new int[]{numberGenerator(), numberGenerator(), numberGenerator(), numberGenerator()}); //creates the answer
-            games.save(master);
-        }
-    }
+//    @PostConstruct
+//    public void init() {
+//        if(games.count() == 0) { //If this is either the start of the game or a new game, this is run to create the answer
+//            Mastermind master = new Mastermind();
+//            master.setRound(0);
+//            master.setGuesses(new int[]{numberGenerator(), numberGenerator(), numberGenerator(), numberGenerator()}); //creates the answer
+//            games.save(master);
+//        }
+//    }
 
     public static int numberGenerator() { // gets a number between 1 - 8 randomly
         return ((int)Math.random() * 8) + 1;
