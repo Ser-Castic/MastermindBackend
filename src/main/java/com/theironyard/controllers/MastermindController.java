207d2fd12d0827e773927ba4ab907759db27741c
Ser-Castic <samsteinberg90@gmail.com>
2017-01-23 12:12:15 -0500

    Added mustache for testing purposes

--- src/main/java/com/theironyard/controllers/MastermindController.java
+++ src/main/java/com/theironyard/controllers/MastermindController.java
@@ -5,6 +5,7 @@ import com.theironyard.services.MastermindRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,9 +36,9 @@ public class MastermindController {
 //
 //    }
 
+    @CrossOrigin
     @RequestMapping(path = "/", method = RequestMethod.GET)
-    public String home(Model model) {
-        model.addAttribute(games.findAll());
+    public String home() {
         return "home";
     }
 
