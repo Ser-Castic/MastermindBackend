4fde9a1784334f482ccb266bec8c2e206ce4ade5
Ser-Castic <samsteinberg90@gmail.com>
2017-01-24 14:02:07 -0500

    Added int [] answer array, to store current answer to be checked

--- src/main/java/com/theironyard/controllers/MastermindController.java
+++ src/main/java/com/theironyard/controllers/MastermindController.java
@@ -6,28 +6,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.Arrays;
 
 @RestController
 public class MastermindController {
+
+    private boolean reset = true;
+    int [] answer = new int[4];
 
     @Autowired
     MastermindRepository games;
 
     @PostConstruct
     public void init() {
-        if(games.count() == 0) { //If this is either the start of the game or a new game, this is run to create the answer
+        if (reset) { //If this is either the start of the game or a new game, this is run to create the answer
             Mastermind master = new Mastermind();
             master.setGuesses(new int[]{numberGenerator(), numberGenerator(), numberGenerator(), numberGenerator()});//creates the answer
-            master.setChecks(new int[]{0,0,0,0});
+            master.setChecks(new int[]{0, 0, 0, 0});
+            answer = master.getGuesses();
             games.save(master);
+            reset = false;
         }
     }
 
     public static int numberGenerator() { // gets a number between 1 - 8 randomly
-        return (int)(Math.random() * 8) + 1;
+        return (int) (Math.random() * 8) + 1;
     }
 
     public static int[] checkAgainstAnswer(int[] answerArray, int[] guessArray) { // this checks
@@ -36,14 +39,14 @@ public class MastermindController {
 
         int[] result = new int[answerArray.length];
 
-        for(int i = 0; i < result.length; i++) {
+        for (int i = 0; i < result.length; i++) {
             if (answerArray[i] == guessArray[i]) {
                 result[i] = 2;
                 answerArray[i] = 0;
                 guessArray[i] = 0;
             }
         }
-        for(int i = 0; i < result.length; i++) {
+        for (int i = 0; i < result.length; i++) {
             int answerIndex = Arrays.binarySearch(answerArray, guessArray[i]);
 
             if (guessArray[i] > 0 && answerIndex > -1) {
@@ -56,17 +59,16 @@ public class MastermindController {
 
     @CrossOrigin
     @RequestMapping(path = "/", method = RequestMethod.GET)
-    public Mastermind home() {
+    public Iterable<Mastermind> home() {
         init();
-        return games.findOne(1); // index(round), guess(Answer), checks(bogus array), round 1 is a wash
+        return games.findAll(); // index(round), guess(Answer), checks(bogus array), round 1 is a wash
     }
 
     @CrossOrigin
     @RequestMapping(path = "/guess", method = RequestMethod.POST)
-    public Iterable<Mastermind> guessCheck(@RequestBody int [] guess) {//request JSON object in the form of int array
-        Mastermind answerObject = games.findOne(1);// sets answer Object to check against the guess
+    public Iterable<Mastermind> guessCheck(@RequestBody int[] guess) {//request JSON object in the form of int array
         Mastermind guessObject = new Mastermind();
-        int [] response = checkAgainstAnswer(answerObject.getGuesses(), guess);//sets the checks array based on checkAgainstAnswer method
+        int[] response = checkAgainstAnswer(answer, guess);//sets the checks array based on checkAgainstAnswer method
         guessObject.setGuesses(guess);
         guessObject.setChecks(response);
         games.save(guessObject); // saves to repo
@@ -74,11 +76,10 @@ public class MastermindController {
     }
 
     @CrossOrigin
-    @RequestMapping(path = "/new-game", method = RequestMethod.GET)
-    void newGame(HttpServletResponse response) throws IOException {
+    @RequestMapping (path = "/new-game", method = RequestMethod.POST)
+    public void newGame (){
+        reset = true;
         games.deleteAll();
-        init();
-        response.sendRedirect("redirect:/");
     }
 
 }
